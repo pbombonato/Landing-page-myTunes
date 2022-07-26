d95bcd390ab7d9e4ac3ab6b2e8cc84ed6e255d76
pbombonato <pablobombonato14@gmail.com>
2022-07-25 22:09:35 -0300

    Tornei o site responsivo!

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
